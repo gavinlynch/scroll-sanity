f208206cc6bdd5c06bf3b2349dfff8fa2352ee41
Gavin Lynch <gavin.b.lynch@gmail.com>
2018-04-18 02:25:47 -0500

    Initial Commit

---
+++ scroll-sanity/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  scroll-sanity
+//
+//  Created by Gavin Lynch on 4/18/18.
+//  Copyright © 2018 Gavin Lynch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
